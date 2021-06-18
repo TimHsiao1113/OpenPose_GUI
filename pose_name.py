8ff291c886a918ece41db69465a565157c88be4b
Tim <lotus2460@gmail.com>
2021-06-18 14:39:03 +0800

    Prototype without transfer image

---
+++ pose_name.py
@@ -0,0 +1,18 @@
+
+
+def mountain(show_name, show_prob):
+	show_name.setText('Mountain Pose')
+	show_prob.setText('1000%')
+	pass
+
+def side():
+	pass
+
+def warrior():
+	pass
+
+def seat_one():
+	pass
+
+def seta_two():
+	pass
